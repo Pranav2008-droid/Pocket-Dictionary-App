45e6ed988824a177e4f28068afb306546c5af27a
pranavrameshk <pranavrameshk@gmail.com>
2020-06-01 18:10:29 +0530

    Finished The Basic FrameWork

---
+++ App.js
@@ -0,0 +1,87 @@
+import * as React from 'react';
+import { Platform, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import CreateTitle from './Components/CreateTitle';
+
+export default class App extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      data: null,
+    };
+  }  
+  retrieveInfo = () =>{
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    var url=`https://whitehat-dictionary.glitch.me/?word=${this.state.text}`;
+    fetch(`${proxyurl}${url}`)
+    .then((response) => response.json())
+    .then((responseJson) =>{
+        this.setState({data : JSON.parse(responseJson)});
+        console.log(this.state);
+      }).catch((error) => {
+        console.log("Error while fetching the url")
+        console.log(error);
+      })
+  }
+  render(){
+    return (
+      <View>
+        <CreateTitle title="Pocket Dictionary"></CreateTitle>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.inputBox}
+            onChangeText={text => {
+              this.setState({ text: text.toLowerCase() });
+            }}
+            value={this.state.text}
+          />      
+          <TouchableOpacity 
+          onPress={this.retrieveInfo}
+          style={styles.button}
+          >
+            <Text style={styles.buttonText}>Check In Dictionary</Text>
+          </TouchableOpacity>
+        </View>
+
+        {this.state.data != null ? <Text>Type: {JSON.stringify(this.state.data.results[0].type).replace(/"/g, "")}</Text> : <View/>}
+        {this.state.data != null ? <Text>Definition: {JSON.stringify(this.state.data.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]).replace(/"/g, "")}</Text> : <View/>}
+
+      </View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#b8b8b8',
+  },
+  inputBox: {
+    marginTop: 50,
+    marginLeft : '5%',
+    width: '80%',
+    alignSelf: 'center',
+    height: 40,
+    textAlign: 'center',
+    borderWidth: 1,
+    outline: 'none',
+  },
+  button: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    borderRadius: 15,
+    marginTop: 50,
+    marginLeft : 20,
+    width: 200,
+    height: 50,
+    backgroundColor : 'blue',
+  },  
+  buttonText : {
+    alignSelf : 'center',
+    color : 'white',
+  },
+  inputContainer : {
+    flexDirection: 'row',
+  }
+});
---
+++ Components/CreateTitle.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+class CreateTitle extends React.Component{
+  render(){
+    return(
+      <View style= {styles.textContainer}>
+        <Text style={styles.text}>{this.props.title}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  textContainer:{
+    backgroundColor: 'blue',
+  },
+  text:{
+    color: 'white',
+    padding: 20,
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
+});
+
+export default CreateTitle;
